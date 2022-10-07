278ca4fa1ed51686e42ff63764b87442d3a329f9
Karen Kanana <kananakaren21@gmail.com>
2022-10-07 08:16:46 +0300

    update AddButton function

---
+++ src/Components/AddButton.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function AddButton() {
+    return (
+      <Link to='/note/new' className='bottom-btn'>
+         <AddIcon />
+      </Link>
+  
+  
+      )
+  }
+  
+  export default AddButton;
